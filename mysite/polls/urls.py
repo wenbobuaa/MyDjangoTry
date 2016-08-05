b95d3874921d17a08f731411bbafd0315faef797
wenbo <Liwenbo@gmei.com>
2016-08-05 18:16:32 +0800

    init3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^(?P<question_id>[0-9]+)/detail$', views.detail, name = 'deatil'),
+    url(r'^(?P<question_id>[0-9]+)/result$', views.result, name = 'result'),
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name = 'result'),
+    
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, World! You're at polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're at {id} question detail.".format(id=question_id))
+
+
+def result(request, question_id):
+    return HttpResponse("You're at {id} question results.".format(id=question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You're at {id} question vote.".format(id=question_id))
+
