20b0fa1358fc3b4cf9af2700dc319615e1f3dc02
wenbo <Liwenbo@gmei.com>
2016-08-09 17:20:07 +0800

    page4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<question_id>[0-9]+)/detail$', views.detail, name = 'deatil'),
+    url(r'^(?P<question_id>[0-9]+)/detail$', views.detail, name = 'detail'),
     url(r'^(?P<question_id>[0-9]+)/result$', views.result, name = 'result'),
-    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name = 'result'),
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name = 'vote'),
     
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+# from django.urls import reverse
+from django.core.urlresolvers import reverse
+
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello, World! You're at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're at {id} question detail.".format(id=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def result(request, question_id):
-    return HttpResponse("You're at {id} question results.".format(id=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/result.html', context)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're at {id} question vote.".format(id=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:result', args=(question_id,)))
 
